b91e2b3434eb66d78afd460f362e8e42ffb80539
Mohamed Tarek Ahmed <mohamedtarekahmed2@gmail.com>
2026-03-17 13:56:06 +0200

    second commit

---
+++ template.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t = 1;
+
+    while (t--) {
+        
+    }
+    return 0;
+}
